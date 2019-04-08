3d9d66e199514cf9523b0c8783ecfef79b7bfd43
CaptainObvious0 <michaelstets1@gmail.com>
2019-04-08 17:43:41 -0500

    InventoryMove - Fixes & Config additions

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/inventory/InventoryMove.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/inventory/InventoryMove.java
@@ -1,6 +1,8 @@
 package fr.neatmonster.nocheatplus.checks.inventory;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryType.SlotType;
 
 import fr.neatmonster.nocheatplus.checks.Check;
 import fr.neatmonster.nocheatplus.checks.CheckType;
@@ -15,14 +17,25 @@ public class InventoryMove extends Check {
         super(CheckType.INVENTORY_MOVE);
     }
 	
-	public boolean check(final Player player, final InventoryData data, final IPlayerData pData) {
+	public boolean check(final Player player, final InventoryData data, final IPlayerData pData, final InventoryConfig cc, final SlotType type) {
 		
 		boolean cancel = false;
-		
-		if (player.isBlocking() || player.isSprinting() || player.isSneaking() || player.isSwimming()) {
+		if (player.isBlocking() || player.isSprinting() || player.isDead() || player.isSneaking() || player.isSwimming()) {
+			if (player.getGameMode() == GameMode.CREATIVE) {
+			if (!cc.invMoveDisableCreative) {
+				if (type == SlotType.QUICKBAR) {
+					return false;
+				} else {
+				  data.invMoveVL += 1D;
+				  cancel = executeActions(player, data.invMoveVL, 1D, pData.getGenericInstance(InventoryConfig.class).invMoveActionList).willCancel();
+				}
+			} else {
+			  return false;
+			}
+			} else {
 			data.invMoveVL += 1D;
-			
-			cancel = executeActions(player, data.invMoveVL, 1D, pData.getGenericInstance(InventoryConfig.class).invMoveActionList).willCancel();
+			cancel = executeActions(player, data.invMoveVL, 1D, pData.getGenericInstance(InventoryConfig.class).invMoveActionList).willCancel();	
+			}
 		}
 		return cancel;
 	}
