cf8be9da54521fd84680b61dd998cda8c4959581
CaptainObvious0 <michaelstets1@gmail.com>
2019-04-07 19:25:20 -0500

    NetConfig - Improbable Weight

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/net/NetConfig.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/net/NetConfig.java
@@ -55,6 +55,7 @@ public class NetConfig extends ACheckConfig {
     public final float attackFrequencyLimitSecondsTwo;
     public final float attackFrequencyLimitSecondsFour;
     public final float attackFrequencyLimitSecondsEight;
+    public final float attackFrequencyImprobableWeight;
     public final ActionList attackFrequencyActions;
     
     public final int fightSyncResetCount;
@@ -92,6 +93,7 @@ public class NetConfig extends ACheckConfig {
         attackFrequencyLimitSecondsTwo = config.getInt(ConfPaths.NET_ATTACKFREQUENCY_SECONDS_TWO);
         attackFrequencyLimitSecondsFour= config.getInt(ConfPaths.NET_ATTACKFREQUENCY_SECONDS_FOUR);
         attackFrequencyLimitSecondsEight = config.getInt(ConfPaths.NET_ATTACKFREQUENCY_SECONDS_EIGHT);
+        attackFrequencyImprobableWeight = (float) config.getDouble(ConfPaths.NET_ATTACKFREQUENCY_IMPROBABLE_WEIGHT);
         attackFrequencyActions = config.getOptimizedActionList(ConfPaths.NET_ATTACKFREQUENCY_ACTIONS, Permissions.NET_ATTACKFREQUENCY);
         
         fightSyncResetCount = config.getInt(ConfPaths.NET_FIGHTSYNC_RESETCOUNT);
