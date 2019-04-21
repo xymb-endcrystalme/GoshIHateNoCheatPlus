50dc07e8a4f06ac72775b335d4a61fc870b29c84
Lysandr0 <gimmy.q33@gmail.com>
2019-04-22 00:02:07 +0200

    Confine the hotbar skip condition to just Creative mode.

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/inventory/InventoryMove.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/inventory/InventoryMove.java
@@ -2,7 +2,6 @@ package fr.neatmonster.nocheatplus.checks.inventory;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ArrayList;
 
 import org.bukkit.GameMode;
 import org.bukkit.event.inventory.InventoryType.SlotType;
@@ -28,95 +27,105 @@ public class InventoryMove extends Check {
         super(CheckType.INVENTORY_MOVE);
     }
 	
-    public boolean check(final Player player, final InventoryData data, final IPlayerData pData, final InventoryConfig cc, final SlotType type) {
+  public boolean check(final Player player, final InventoryData data, final IPlayerData pData, final InventoryConfig cc, final SlotType type) {
 		
     boolean cancel = false;
     boolean violation = false;
+    
     tags.clear();
-    // If the player is interacting with their hotbar, then skip the check
-    if (type == SlotType.QUICKBAR) {
-        return false;
-    }
-    else {
-    	// Inv. interaction whilst blocking
-		if (player.isBlocking()) {
-		    // Is the player in Creative?
-		   if (player.getGameMode() == GameMode.CREATIVE) {
-		        // Is the DisableCreative config option not enabled?
-				if (!cc.invMoveDisableCreative) {
-			         // Then trigger a violation and add a tag displaying what kind of interaction the player tried to perform
-					tags.add("isBlocking");
-					violation = true;
-			    }
-			    // If it's enabled, skip
-			    else {
-			      return false;
-
-			    }
+ 	// Inv. interaction whilst blocking
+	if (player.isBlocking()) {
+	  // Check if the player is in creative
+	  if (player.getGameMode() == GameMode.CREATIVE) {
+		  // (Skip hotbar interactions)
+		  if(type == SlotType.QUICKBAR) {
+		    	return false; 
 		  }
-		    // Player is not in creative, trigger a violation + add tag
-		    else {
-			  tags.add("isBlocking");
-			  violation = true;
-			}
-	   }
+		  // Check if the Disable_Creative option is not enabled
+		  else if (!cc.invMoveDisableCreative) {
+			   // Trigger a violation + tags
+		           tags.add("isBlocking");
+		           violation = true;
+		  }
+		  // Disable_Creative is enabled, skip
+		  else {
+		    return false;
+		  }
+	  }
+	  // Player is not in creative 
+	  else {
+	    tags.add("isBlocking");
+	    // Trigger a violation + tags
+	    violation = true;
+          }
+        }
      	// Inv. interaction whilst swimming
-		else if (player.isSwimming()) {
-			if (player.getGameMode() == GameMode.CREATIVE) {
-				if (!cc.invMoveDisableCreative) {
-			            tags.add("isSwimming");
-			            violation = true;
-			        }
-			         else {
-			          return false;
-			         }
-		        }
-		        else {
-		         tags.add("isSwimming");
-		         violation = true;
-	               }
-	        }
-    	// Inv. interaction whilst sprinting
-		else if (player.isSprinting()) {
-			if (player.getGameMode() == GameMode.CREATIVE) {
-				if (!cc.invMoveDisableCreative) {
-			            tags.add("isSprinting");
-			            violation = true;
-			        }
-			         else {
-			          return false;
-			         }
-		        }
-		        else {
-		         tags.add("isSprinting");
-		         violation = true;
-	               }
-	        }
-     	// Inv. interaction whilst sneaking
-		else if (player.isSneaking()) {
-			if (player.getGameMode() == GameMode.CREATIVE) {
-				if (!cc.invMoveDisableCreative) {
-			            tags.add("isSneaking");
-			            violation = true;
-			        }
-			         else {
-			          return false;
-			         }
-		        }
-		        else {
-		         tags.add("isSneaking");
-		         violation = true;
-	               }
-	        }
+	else if (player.isSwimming()) {
+		  if (player.getGameMode() == GameMode.CREATIVE) {
+			 if(type == SlotType.QUICKBAR) {
+			    	return false; 
+			 }
+			 else if (!cc.invMoveDisableCreative) {
+			           tags.add("isSwimming");
+			           violation = true;
+			 }
+			 else {
+			    return false;
+			 }
+		  }
+	         else {
+	          tags.add("isBlocking");
+	          violation = true;
+                 }
+        }
+     	// Inv. interaction whilst sprinting
+	else if (player.isSprinting()) {
+		  if (player.getGameMode() == GameMode.CREATIVE) {
+			 if(type == SlotType.QUICKBAR) {
+			    	return false; 
+			 }
+			 else if (!cc.invMoveDisableCreative) {
+			           tags.add("isSprinting");
+			           violation = true;
+			 }
+			 else {
+			    return false;
+			 }
+		  }
+		  else {
+		    tags.add("isSprinting");
+		    violation = true;
+	          }
+	}
+     	// Inv. interaction whilst Sneaking
+	else if (player.isSneaking()) {
+		  if (player.getGameMode() == GameMode.CREATIVE) {
+			 if(type == SlotType.QUICKBAR) {
+			    	return false; 
+			 }
+			 else if (!cc.invMoveDisableCreative) {
+			           tags.add("isSneaking");
+			           violation = true;
+			 }
+			 else {
+			    return false;
+			 }
+		  }
+		  else {
+		    tags.add("isSneaking");
+		    violation = true;
+	           }
+	}
 	    if (violation) {
 	       data.invMoveVL += 1.0D;
 	       final ViolationData vd = new ViolationData(this, player, data.invMoveVL, 1, pData.getGenericInstance(InventoryConfig.class).invMoveActionList);
 	    if (vd.needsParameters()){
 	        vd.setParameter(ParameterName.TAGS, StringUtil.join(tags, "+"));
 	    }
-	  return executeActions(vd).willCancel();
+	    return executeActions(vd).willCancel();
 	  } 
 	return cancel;
        }
      }
-  }
+  
+
