ada91e81cf7380fc0372fcca0add709ea96a556a
xaw3ep <51277581+xaw3ep@users.noreply.github.com>
2019-07-06 14:13:39 +0700

    Add blocks define for Minecraft 1.14

---
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/compat/blocks/init/vanilla/BlocksMC1_14.java
@@ -0,0 +1,153 @@
+package fr.neatmonster.nocheatplus.compat.blocks.init.vanilla;
+
+import org.bukkit.Material;
+
+import fr.neatmonster.nocheatplus.compat.BridgeMaterial;
+import fr.neatmonster.nocheatplus.compat.blocks.BlockPropertiesSetup;
+import fr.neatmonster.nocheatplus.compat.blocks.init.BlockInit;
+import fr.neatmonster.nocheatplus.config.WorldConfigProvider;
+import fr.neatmonster.nocheatplus.logging.StaticLog;
+import fr.neatmonster.nocheatplus.utilities.map.BlockFlags;
+import fr.neatmonster.nocheatplus.utilities.map.BlockProperties;
+
+public class BlocksMC1_14 implements BlockPropertiesSetup{
+
+	public BlocksMC1_14() {
+        BlockInit.assertMaterialExists("LECTERN");
+        BlockInit.assertMaterialExists("STONECUTTER");
+    }
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void setupBlockProperties(WorldConfigProvider<?> worldConfigProvider) {
+		// TODO Auto-generated method stub
+		BlockFlags.addFlags("VINE", BlockProperties.F_CLIMBUPABLE);
+		final long stepFlags = BlockProperties.F_GROUND | BlockProperties.F_XZ100 | BlockProperties.F_GROUND_HEIGHT;
+		final long wall = BlockProperties.F_VARIABLE | BlockProperties.F_GROUND | BlockProperties.F_HEIGHT150 | BlockProperties.F_THICK_FENCE;
+		final BlockProperties.BlockProps instant = BlockProperties.instantType;
+		BlockInit.setPropsAs("END_STONE_BRICK_WALL", BridgeMaterial.END_STONE_BRICKS);
+		BlockFlags.addFlags("END_STONE_BRICK_WALL", wall);
+		BlockInit.setPropsAs("END_STONE_BRICK_STAIRS", BridgeMaterial.END_STONE_BRICKS);
+		BlockFlags.setFlagsAs("END_STONE_BRICK_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setPropsAs("END_STONE_BRICK_SLAB", BridgeMaterial.END_STONE_BRICKS);
+		BlockFlags.addFlags("END_STONE_BRICK_SLAB", stepFlags);
+		
+		BlockInit.setPropsAs("SANDSTONE_WALL", Material.SANDSTONE);
+		BlockFlags.addFlags("SANDSTONE_WALL", wall);
+		BlockInit.setPropsAs("SANDSTONE_STAIRS", Material.SANDSTONE);
+		BlockFlags.setFlagsAs("SANDSTONE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setPropsAs("SMOOTH_SANDSTONE_SLAB", Material.SANDSTONE);
+		BlockFlags.addFlags("SMOOTH_SANDSTONE_SLAB", stepFlags);
+		BlockInit.setPropsAs("CUT_SANDSTONE_SLAB", Material.SANDSTONE);
+		BlockFlags.addFlags("CUT_SANDSTONE_SLAB", stepFlags);
+		BlockInit.setPropsAs("SMOOTH_SANDSTONE_STAIRS", Material.SANDSTONE);
+		BlockFlags.setFlagsAs("SMOOTH_SANDSTONE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setPropsAs("RED_SANDSTONE_WALL", Material.SANDSTONE);
+		BlockFlags.addFlags("RED_SANDSTONE_WALL", wall);
+		BlockInit.setPropsAs("RED_SANDSTONE_STAIRS", Material.SANDSTONE);
+		BlockFlags.setFlagsAs("RED_SANDSTONE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setPropsAs("SMOOTH_RED_SANDSTONE_STAIRS", Material.SANDSTONE);
+		BlockFlags.setFlagsAs("SMOOTH_RED_SANDSTONE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setPropsAs("SMOOTH_RED_SANDSTONE_SLAB", Material.SANDSTONE);
+		BlockFlags.addFlags("SMOOTH_RED_SANDSTONE_SLAB", stepFlags);
+		BlockInit.setPropsAs("CUT_RED_SANDSTONE_SLAB", Material.SANDSTONE);
+		BlockFlags.addFlags("CUT_RED_SANDSTONE_SLAB", stepFlags);
+		
+		BlockInit.setAs("RED_NETHER_BRICK_WALL", BridgeMaterial.COBBLESTONE_WALL);
+		BlockInit.setAs("RED_NETHER_BRICK_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setAs("RED_NETHER_BRICK_SLAB", BridgeMaterial.STONE_SLAB);
+		BlockFlags.addFlags("RED_NETHER_BRICK_SLAB", stepFlags);
+		BlockInit.setAs("NETHER_BRICK_WALL", BridgeMaterial.COBBLESTONE_WALL);
+		BlockInit.setAs("MOSSY_STONE_BRICK_WALL", BridgeMaterial.COBBLESTONE_WALL);
+		BlockInit.setAs("MOSSY_STONE_BRICK_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setAs("MOSSY_STONE_BRICK_SLAB", BridgeMaterial.STONE_SLAB);
+		BlockFlags.addFlags("MOSSY_STONE_BRICK_SLAB", stepFlags);
+		BlockInit.setAs("STONE_BRICK_WALL", BridgeMaterial.COBBLESTONE_WALL);
+		BlockInit.setAs("MOSSY_COBBLESTONE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setAs("MOSSY_COBBLESTONE_SLAB", BridgeMaterial.STONE_SLAB);
+		BlockFlags.addFlags("MOSSY_COBBLESTONE_SLAB", stepFlags);
+		BlockInit.setAs("PRISMARINE_WALL", BridgeMaterial.COBBLESTONE_WALL);
+		BlockInit.setAs("GRANITE_WALL", BridgeMaterial.COBBLESTONE_WALL);
+		BlockInit.setAs("GRANITE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setAs("POLISHED_GRANITE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setAs("GRANITE_SLAB", BridgeMaterial.STONE_SLAB);
+		BlockInit.setAs("POLISHED_GRANITE_SLAB", BridgeMaterial.STONE_SLAB);
+		BlockFlags.addFlags("GRANITE_SLAB", stepFlags);
+		BlockFlags.addFlags("POLISHED_GRANITE_SLAB", stepFlags);
+		BlockInit.setAs("DIORITE_WALL", BridgeMaterial.COBBLESTONE_WALL);
+		BlockInit.setAs("DIORITE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setAs("POLISHED_DIORITE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setAs("DIORITE_SLAB", BridgeMaterial.STONE_SLAB);
+		BlockInit.setAs("POLISHED_DIORITE_SLAB", BridgeMaterial.STONE_SLAB);
+		BlockFlags.addFlags("DIORITE_SLAB", stepFlags);
+		BlockFlags.addFlags("POLISHED_DIORITE_SLAB", stepFlags);
+		BlockInit.setAs("ANDESITE_WALL", BridgeMaterial.COBBLESTONE_WALL);
+		BlockInit.setAs("ANDESITE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setAs("POLISHED_ANDESITE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setAs("ANDESITE_SLAB", BridgeMaterial.STONE_SLAB);
+		BlockInit.setAs("POLISHED_ANDESITE_SLAB", BridgeMaterial.STONE_SLAB);
+		BlockFlags.addFlags("ANDESITE_SLAB", stepFlags);
+		BlockFlags.addFlags("POLISHED_ANDESITE_SLAB", stepFlags);
+		BlockInit.setAs("BRICK_WALL", BridgeMaterial.COBBLESTONE_WALL);
+		BlockInit.setAs("STONE_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setAs("SMOOTH_QUARTZ_STAIRS", BridgeMaterial.STONE_BRICK_STAIRS);
+		BlockInit.setAs("SMOOTH_STONE_SLAB", BridgeMaterial.STONE_SLAB);
+		BlockInit.setAs("SMOOTH_QUARTZ_SLAB", BridgeMaterial.STONE_SLAB);
+		BlockFlags.addFlags("SMOOTH_STONE_SLAB", stepFlags);
+		BlockFlags.addFlags("SMOOTH_QUARTZ_SLAB", stepFlags);
+		
+		BlockInit.setAs("LOOM", BridgeMaterial.CRAFTING_TABLE);
+		BlockInit.setAs("FLETCHING_TABLE", BridgeMaterial.CRAFTING_TABLE);
+		BlockInit.setAs("SMITHING_TABLE", BridgeMaterial.CRAFTING_TABLE);
+		BlockInit.setAs("CARTOGRAPHY_TABLE", BridgeMaterial.CRAFTING_TABLE);
+		BlockInit.setAs("JIGSAW", BridgeMaterial.COMMAND_BLOCK);		
+		BlockInit.setAs("BLAST_FURNACE", Material.FURNACE);
+		BlockInit.setAs("SMOKER", Material.FURNACE);
+		BlockInit.setAs("COMPOSTER", Material.DIRT);
+		BlockInit.setAs("LECTERN", Material.OAK_PLANKS);
+		BlockFlags.addFlags("LECTERN", BlockProperties.F_MIN_HEIGHT16_14);
+		BlockFlags.addFlags("LECTERN", BlockProperties.F_MIN_HEIGHT8_1);
+		BlockFlags.addFlags("LECTERN", BlockProperties.F_GROUND_HEIGHT);
+		BlockFlags.addFlags("LECTERN", BlockProperties.F_GROUND);
+		BlockInit.setAs("BARREL", Material.OAK_PLANKS);
+        BlockProperties.setBlockProps("SCAFFOLDING", instant);
+        BlockFlags.addFlags("SCAFFOLDING", BlockProperties.F_IGN_PASSABLE);
+        BlockFlags.addFlags("SCAFFOLDING", BlockProperties.F_GROUND | BlockProperties.F_GROUND_HEIGHT);
+        BlockFlags.addFlags("SCAFFOLDING", BlockProperties.F_CLIMBABLE);
+        BlockInit.setAs("STONECUTTER", Material.BRICK_SLAB);
+		BlockFlags.addFlags("STONECUTTER", BlockProperties.F_MIN_HEIGHT16_9);
+		BlockFlags.addFlags("STONECUTTER", BlockProperties.F_GROUND_HEIGHT);
+		BlockProperties.setBlockProps("BAMBOO", new BlockProperties.BlockProps(BlockProperties.noTool, 0.7f, BlockProperties.secToMs(1.25)));
+		BlockProperties.setBlockProps("BAMBOO_SAPLING", new BlockProperties.BlockProps(BlockProperties.noTool, 0.7f, BlockProperties.secToMs(1.25)));
+		BlockFlags.addFlags("BAMBOO", BlockProperties.F_IGN_PASSABLE | BlockProperties.F_GROUND | BlockProperties.F_GROUND_HEIGHT | BlockProperties.F_THIN_FENCE);
+		BlockFlags.addFlags("BAMBOO_SAPLING", BlockProperties.F_IGN_PASSABLE);
+		BlockFlags.addFlags("WITHER_ROSE", BlockProperties.F_IGN_PASSABLE);
+		BlockProperties.setBlockProps("WITHER_ROSE", instant);		
+		BlockFlags.addFlags("CORNFLOWER", BlockProperties.F_IGN_PASSABLE);
+		BlockProperties.setBlockProps("CORNFLOWER", instant);
+		BlockFlags.addFlags("LILY_OF_THE_VALLEY", BlockProperties.F_IGN_PASSABLE);
+		BlockProperties.setBlockProps("LILY_OF_THE_VALLEY", instant);
+		BlockInit.setAs("ACACIA_WALL_SIGN", BridgeMaterial.SIGN);
+		BlockInit.setAs("BIRCH_SIGN", BridgeMaterial.SIGN);
+		BlockInit.setAs("BIRCH_WALL_SIGN", BridgeMaterial.SIGN);
+		BlockInit.setAs("DARK_OAK_SIGN", BridgeMaterial.SIGN);
+		BlockInit.setAs("DARK_OAK_WALL_SIGN", BridgeMaterial.SIGN);
+		BlockInit.setAs("JUNGLE_SIGN", BridgeMaterial.SIGN);
+		BlockInit.setAs("JUNGLE_WALL_SIGN", BridgeMaterial.SIGN);
+		BlockInit.setAs("OAK_SIGN", BridgeMaterial.SIGN);
+		BlockInit.setAs("OAK_WALL_SIGN", BridgeMaterial.SIGN);
+		BlockInit.setAs("SPRUCE_SIGN", BridgeMaterial.SIGN);
+		BlockInit.setAs("SPRUCE_WALL_SIGN", BridgeMaterial.SIGN);
+		BlockInit.setAs("GRINDSTONE", Material.COBBLESTONE);
+		BlockFlags.addFlags("GRINDSTONE", BlockProperties.F_GROUND | BlockProperties.F_GROUND_HEIGHT);
+		BlockInit.setAs("CAMPFIRE", Material.OAK_PLANKS);
+		BlockFlags.addFlags("CAMPFIRE", BlockProperties.F_MIN_HEIGHT16_7);
+		BlockFlags.addFlags("CAMPFIRE", BlockProperties.F_GROUND_HEIGHT);
+		BlockProperties.setBlockProps("BELL", new BlockProperties.BlockProps(BlockProperties.woodPickaxe, 7f, BlockProperties.secToMs(23.0, 3.8, 2.2, 1, 1.5, 0.8)));
+		BlockProperties.setBlockProps("LANTERN", new BlockProperties.BlockProps(BlockProperties.woodPickaxe, 6.0f, BlockProperties.secToMs(17.0, 2.8, 1.8, 0.7, 0.8, 0.5)));
+		BlockFlags.addFlags("LANTERN", BlockProperties.F_GROUND | BlockProperties.F_GROUND_HEIGHT);
+		BlockFlags.addFlags("SWEET_BERRY_BUSH", BlockProperties.F_COBWEB);
+		StaticLog.logInfo("Added block-info for Minecraft 1.14 blocks.");
+	}
+
+}
