5050724f6f78f51fff928fb5d914f4211a59c556
Xaw3ep <thuan3443g@gmail.com>
2019-10-14 20:30:38 +0700

    Patch elytra+ bypasses
    Hmm, forgot to check eyeheight and change y while gliding

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/player/CreativeFly.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/checks/moving/player/CreativeFly.java
@@ -140,6 +140,8 @@ public class CreativeFly extends Check {
         double limitH = resH[0];
         double resultH = resH[1];
 
+        resultH = Math.max(resultH, hackElytraH(hDistance, yDistance, thisMove, lastMove, data, player));
+	    
         // Check velocity.
         if (resultH > 0) {
             double hFreedom = data.getHorizontalFreedom();
@@ -490,6 +492,20 @@ public class CreativeFly extends Check {
         return new double[] {limitV, resultV};
     }
 
+    private double hackElytraH(final double hDistance, final double yDistance, final PlayerMoveData thisMove, final PlayerMoveData lastMove, final MovingData data, final Player player) {
+        if (Bridge1_9.isGlidingWithElytra(player)) {
+            if (Math.abs(yDistance) <= (data.liqtick == 0 ? 0.0001 : -1.0) && data.fireworksBoostDuration == 0) {
+                tags.add("elytra_no_y");
+                return hDistance < 1.0 ? hDistance * 2.0 : hDistance;
+            }
+            if (player.getEyeHeight() > 0.45 && hDistance > 0.5) {
+                tags.add("elytra_wrongheight");
+                return hDistance < 1.0 ? hDistance * 2.0 : hDistance;
+            }
+        }
+        return 0.0;
+    }
+	
     private double hackLytra(final double yDistance, final double limitV, final PlayerMoveData thisMove, final PlayerMoveData lastMove, final MovingData data) {
         // TODO: Hack, move / config / something.
         // TODO: Confine more. hdist change relates to ydist change
